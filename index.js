299ffaec048373433728218448892f43a30442f7
Tejas99999 <59287568+Tejas99999@users.noreply.github.com>
2020-05-05 15:16:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+var randomnumber1=Math.floor(Math.random()*6+1);
+// alert(randomnumber1);
+if(randomnumber1===1)
+document.querySelector(".img1").setAttribute("src","images/dice1.png");
+else if(randomnumber1===2)
+document.querySelector(".img1").setAttribute("src","images/dice2.png");
+else if(randomnumber1===3)
+document.querySelector(".img1").setAttribute("src","images/dice3.png");
+else if(randomnumber1===4)
+document.querySelector(".img1").setAttribute("src","images/dice4.png");
+else if(randomnumber1===5)
+document.querySelector(".img1").setAttribute("src","images/dice5.png");
+else if(randomnumber1===6)
+document.querySelector(".img1").setAttribute("src","images/dice6.png");
+
+
+var randomnumber2=Math.floor(Math.random()*6+1);
+// alert(randomnumber1);
+if(randomnumber2===1)
+document.querySelector(".img2").setAttribute("src","images/dice1.png");
+else if(randomnumber2===2)
+document.querySelector(".img2").setAttribute("src","images/dice2.png");
+else if(randomnumber2===3)
+document.querySelector(".img2").setAttribute("src","images/dice3.png");
+else if(randomnumber2===4)
+document.querySelector(".img2").setAttribute("src","images/dice4.png");
+else if(randomnumber2===5)
+document.querySelector(".img2").setAttribute("src","images/dice5.png");
+else if(randomnumber2===6)
+document.querySelector(".img2").setAttribute("src","images/dice6.png");
+
+if(randomnumber1>randomnumber2)
+document.querySelector("h1").innerHTML="Player1 wins";
+else if(randomnumber1<randomnumber2)
+document.querySelector("h1").innerHTML="Player2 wins";
+else
+document.querySelector("h1").innerHTML="Its A Tie";
+
+
+
+
+// document.querySelector("img").setAttribute("src","images/dice4.png");
